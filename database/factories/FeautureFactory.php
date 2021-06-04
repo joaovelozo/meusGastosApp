91891dba904f8efaa497c9d9f135edee6e6b5780
joaovelozo <joaovelozo@gmail.com>
2021-06-04 04:45:11 -0300

    first commit

---
+++ database/factories/FeautureFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Feauture;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FeautureFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Feauture::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
